a3e6fde9d7e76377956620047bbdc5cfea5f9cd1
Oren Adler <oren106@gmail.com>
2011-12-21 21:12:22 +0200

    changed main page and added a new class that define a product

---
+++ RPS1/App_Code/Interpartor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+using System.Web.UI.WebControls;
+
+/// <summary>
+/// Summary description for Interpartor
+/// </summary>
+public class Interpartor
+{
+   //The interpartor recives the input from the user and split the string and then 
+    //sends the plit string to the class inchsrge of retrivig the data from the data base.
+    prodInfo product;
+    public Interpartor(prodInfo prod)
+    {
+        product = new prodInfo(prod);
+    }
+
+    //will return the answer 
+    public string getAns()
+    {
+       
+        return product.toString();
+    }
+
+    
+
+}
