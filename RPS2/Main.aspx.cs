ad55ed2e00699633e38c0ce110833d995a5d74b7
Oren Adler <oren106@gmail.com>
2011-12-22 15:58:43 +0200

    seacond milestone

---
+++ RPS2/Main.aspx.cs
@@ -0,0 +1,25 @@
+﻿//The code for the main page
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+    public partial class _Default : System.Web.UI.Page
+    {
+        private prodInfo product;
+        private Interpartor inter;
+        protected string text;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        //what to do when the user clicks the ask button 
+        protected void send_Click(object sender, EventArgs e)
+        {
+
+            product = new prodInfo(TextName.Text, TextPlace.Text, TextCity.Text, Convert.ToInt32(TextPrice.Text), Convert.ToInt16(DropDownCurrncy.SelectedValue), Convert.ToInt16(DropDownMessurment.SelectedValue), Convert.ToInt32(TextAmount.Text));
+            inter = new Interpartor(product);
+            Response.Redirect("Answer.aspx?" + inter.getAns());
+        }
+}
